6aeca0964fd142c8393ecb26ec76ca9930242567
Sagar Pandav <pandavsagar123@gmail.com>
2017-08-21 00:44:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,26 @@
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    //Create a Reqest object
+    var request = new XMLHttpRequest();
+    
+    //Capture response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not done yet
+    } ;
+    
+    //Make the request
+    request.open('GET',"http://pandavsagar123.imad.hasura-app.io/counter",true)
+    request.send(null); 
 };
 
 
