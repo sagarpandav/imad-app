e2fbc6cca1b9187f94289bd045f60eadcf129594
Sagar Pandav <pandavsagar123@gmail.com>
2017-08-21 01:07:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,6 +23,25 @@ button.onclick = function(){
     request.send(null); 
 };
 
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function(){
+  //Make req to server and send the names
+  //Capture list of names and renders it as a list  
+  var names = ['name1','name2','name3','name4'];
+  var list = '';    
+  
+  for(var i =0; i<names.length; i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+  
+};
 
 
 
